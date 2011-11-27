b0b38bc4eb54dc0a7dcedd4ffb55d3993631c8b0
Atul Raut <rauji.raut@gmail.com>
2011-11-27 15:13:20 +0530

    Ch-04 : 4.1 R-Book Completed.
    Find Pattern In String

--- r-book/ch-04/4.1/main.c
+++ r-book/ch-04/4.1/main.c
@@ -1,37 +1,45 @@
 /*
+*
 * Find pattern in string.
 * Data : Sunday, September 11 2011 11:02 AM
+* Restarted on Nov 26th-11-2011
 * By : Atul R. Raut
+* A
 ***/
+
 #include <stdio.h>
 #define MAXLINE 1000 /* maximum input line length. */
 
 int getLine (char line[], int max, FILE *fr);
 int strIndex (char souce[], char searchfor[]);
 
-char pattern [] = "ould";    /* pattern to search for*/
+char pattern [] = "ould";    /* PATTERN TO SEARCH FOR*/
 
 int main () {
 
   char c;
-  char line [MAXLINE] = ;
+  char line [MAXLINE] ;
+  int iLine = 0;
   int found = 0;
   FILE *fr = NULL;
   FILE *fw = NULL;
   fr = fopen ("at.txt", "r");
   fw = fopen ("wt.txt", "w");
   if (fr == NULL || fw == NULL) {
-    printf ("\nFile can't open !");
+    printf ("\n File can't open !");
     exit (0);
   }
-
+  /* 
+   * Program Logic based on : 
+   **/
   // while (there's another line)
   //   if (the line contains the pattern)
   //        print it
-   
-  while ( getLine (line, MAXLINE, fr) > 0 ) {
+  while ( iLine = getLine (line, MAXLINE, fr) > 0 ) {
+    //printf ("\niLine = %d", iLine);
+    //    printf ("\nline = %s", line);
     if (strIndex (line, pattern) >= 0)
-      printf ("%s", line);
+      printf ("\n\t Found Line is = %s\n", line);
     found++;
   }
 
@@ -43,13 +51,33 @@ int main () {
 
 /* getLine : get line into s, return length */
 int getLine (char s[], int lim, FILE *fr) {
-  int i, j, k;
-  while ()
+  int c, i;
+  i = 0;
+    
+  while (--lim>0 && (c=getc(fr)) != EOF && c !='\n') {
+    //    printf ("\n c >>>>> %c\n", c);
+    s[i] = c;
+    i++;
+  }
+  if (c == '\n') {
+    s[i] = '\0';
+    i++;
+  }
+    return i; 
 }
 
 /* strIndex : return Index of t in s, -1 if none */
-int strIndex (char souce[], char searchfor[]) {
+int strIndex (char source[], char searchFor[]) {
+  int i, j, k, found;
+  found = -1;
 
+  for (i=0; source[i] != '\0'; i++) {   
+    for (k=0, j=i; searchFor[k] != '\0' && source[j] == searchFor[k]; j++, k++)
+      ;
+    if (k >=0 && searchFor[k] == '\0')
+      return i;
+  } // 1st while end
+  return found;
 }
 
 
