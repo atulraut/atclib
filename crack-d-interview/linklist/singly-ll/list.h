46d6d666158597b2729435e9c39a524267fa2b28
Atul Raut <rauji.raut@gmail.com>
2012-03-30 17:29:00 +0530

    Added some more functions on singly link list.

--- crack-d-interview/linklist/singly-ll/list.h
+++ crack-d-interview/linklist/singly-ll/list.h
@@ -13,9 +13,10 @@ void m_Display();
 void m_Addfirst(int);
 void m_Addlast(int);
 void m_Insert(int,int);
-void m_delfirst();
+void m_Delfirst();
 void m_Deletenode(int);
 void m_Dellast();
+void m_Printrev (NODE *);
 void m_Freelist();
 
 #endif
