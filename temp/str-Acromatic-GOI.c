dba5e093c89805a47bdb8ba335bfc01398fc34bc
Atul Raut <rauji.raut@gmail.com>
2012-04-02 14:39:11 +0530

    Completed function str_acromate in file, str-Acromatic-GOI.c

--- temp/str-Acromatic-GOI.c
+++ temp/str-Acromatic-GOI.c
@@ -3,57 +3,84 @@
 *        : I/P : "Goverment of India" O/P : "GoI";
 * Author : Atul Ramesh Raut
 * Date   : March 30, 2012 08:47PM
-*
+* End    : April 02, 2012 02.30PM
 ***/
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void strAcromatic (char *);
-void org_strAcromatic (char *);
+void strAcromatic (char *, int);
+void org_strAcromatic (char *, int);
 
 int main () {
-  char arr[30] = "Goverment of India";
-  strAcromatic (arr);
+  char acr_str[30] = "Goverment of India";
+  char *acr = acr_str;
+  int acr_len = 0;
+  int flag= 0;
+
+  while (*acr != '\0') {
+    while (*acr != ' ') {
+	    flag++;
+	    acr++;
+	    if (*acr == '\0')
+		    break;
+    }
+    if (flag > 0)
+	    acr_len++;
+    else
+	    flag = 0;
+      acr++;
+  }
+  acr_len++;
+  printf ("\nAT :: acr_len = %d\n", acr_len);
+//  strAcromatic (acr_str, acr_len);
+  org_strAcromatic (acr_str, acr_len);
 }
 
-void strAcromatic (char *s) {
+void strAcromatic (char *s, int str_len) {
   char *t;
-  while (*s != '\0') {    
+
+  while (*s != '\0') {
     *t = *s;
     printf ("\n t = %s t = %p\n", t, t);
-    while (*s != ' ') { // Moved til end of space      
-      s++;
-    }  // 2nd while endohookkkfa   
+    while (*s != ' ') { // Moved til end of space
+	    s++;
+	    if (*s == '\0')
+		    break;
+    }  // 2nd while end
       s++;
       t++;
-  } // 1st while   
+  } // 1st while
   *t = '\0';
-  printf ("\nO/P = %s\n", t);
+  while (str_len > 1) {
+	  t--;
+	  str_len--;
+  }
+  printf ("\n O/P without Malloc = %s !!!\n", t);
 }
 
-void org_strAcromatic (char *s) {
+void org_strAcromatic (char *s, int str_len) {
   char *t;
-  int flag  = 0;
-  t = (char*)malloc(sizeof (5));
+  int i;
+
+  t = (char*)malloc(sizeof (str_len));
+
   while (*s != '\0') {
-    *t = *s;          // collect first character in pointer
-    printf ("\n t = %s t = %p\n", t, t);
-    while (*s != ' ') { // Moved til end of space
-      flag = 1;
-      s++;
-    }  // 2nd while endohookkkfa
-    if (flag == 1) {    
+	  i++;
+	  *t = *s;
+    printf ("\n AT :: i[%d], t = %s t = %p, s=%s\n", i, t, t, s);
+    while (*s != ' ') {
+	    s++;
+	    if (*s == '\0')
+		    break;
+    }
       s++;
       t++;
-    } else {
-      flag = 0;
-      *t = '\0';
-      printf ("\n new t =%p\n", t);
-    }    
-  } // 1st while end 
-  
-  //  t++;
-  //*t = '\0';
-  printf ("\nO/P = %s\n", t);
-}
+  }
+  *t = '\0';
+  while (str_len > 1) {
+	  t--;
+	  str_len--;
+  }
+  printf ("\n With Malloc : new t =%s, t=%p\n", t, t);
+ }
