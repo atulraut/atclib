54ffa7a7cb7d4fd1bb03e4177634e231b8ea466c
Atul Raut <rauji.raut@gmail.com>
2012-08-13 09:31:48 +0530

    denis/r-sol/ch-05/05-06/ : Implemented functions -
    int readlines (char *lineptr[], int nlines, FILE *fr)
    int getLine (char s[], int lim, FILE *fr);

---
+++ temp/char-cmp.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int main () {
+  int i=0;
+  char str1 = 'A';
+  char str2 = 'Z';
+  char str3 = 'a';
+  char str4 = 'z';
+  i = atoi (&str1);
+  //i = strcmp (str1, str2);
+  printf ("\ni = %d str1=%d, str2=%d\n", i, str1, str2);
+  printf ("\nstr3=%d, str4=%d\n", str3, str4);
+  if (str1 == str2)
+    print()
+  return 0;
+}
