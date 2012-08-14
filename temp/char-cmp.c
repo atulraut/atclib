8cc177d73831535aeef56b460563f9c583b49765
Atul Raut <rauji.raut@gmail.com>
2012-08-14 08:46:51 +0530

    temp: Modified char-cmp.c

--- temp/char-cmp.c
+++ temp/char-cmp.c
@@ -5,6 +5,9 @@
 
 int main () {
   int i=0;
+  char arr[4] = {
+    'a', 'b', 'c', 'd'
+  };
   char str1 = 'A';
   char str2 = 'Z';
   char str3 = 'a';
@@ -13,7 +16,19 @@ int main () {
   //i = strcmp (str1, str2);
   printf ("\ni = %d str1=%d, str2=%d\n", i, str1, str2);
   printf ("\nstr3=%d, str4=%d\n", str3, str4);
+  
   if (str1 == str2)
-    print()
+    printf ("\nIsEqual = \n");
+  else if (str1 > str2)
+    printf ("\nIs  > \n");
+  else
+    printf ("\nIs < \n");
+  for (i=0; i<4; i++) {
+    if (arr[i] == str2)
+      printf ("\nIsEqual = \n");
+    else if (str1 > str2)
+      printf ("\nIs  > \n");
+    else
+      printf ("\nIs < \n");
   return 0;
 }
