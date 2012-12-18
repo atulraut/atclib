899174530057c8932aa7cad23c66f8ecdade218f
Atul R. Raut <atulraut17@gmail.com>
2012-12-18 09:08:42 +0530

    New Generic C Implementation & some denis code rewrite.

--- denis/r-sol/ch-05/05-06/main.c
+++ denis/r-sol/ch-05/05-06/main.c
@@ -1,6 +1,6 @@
 /*
 * Array of Pointer  : char *lineptr [MAXLINE]
-* Point to an Array : char (*lineptr) [MAXLINE]
+* Pointer to an Array : char (*lineptr) [MAXLINE]
 * Aim : Sort Input line
 * Logic :
 *         read all the lines of input
@@ -9,6 +9,7 @@
 * ASCII Code for String :
 * a] A-Z ===> 65-91
 * b] a-z ===> 97-123
+* 
 ***/
 #include <stdio.h>
 #include <string.h>
@@ -29,7 +30,7 @@ int main () {
   char c;
   char line [MAXLINES];
   int nlines; /* number of input lines read*/
-  int iLine = 0;
+  int iLine = 0; int i=0;
   int found = 0;
   FILE *fr = NULL;
   FILE *fw = NULL;
@@ -43,12 +44,14 @@ int main () {
   if ( (nlines = readlines (lineptr, MAXLINES, fr)) >= 0) {
     qsort_n (lineptr, 0, nlines-1);
     writelines (lineptr, nlines);
-    return 0;
+    //return 0;
   } else {
     printf ("\nerror: input line too big to handled.\n");
-    return 1;
+    //return 1;
   }
-
+  for (i=0; i<nlines; i++) {
+    printf ("\n lineptr[%d]=%s\n", i, lineptr[i]);
+  }
    // close file
   fclose (fr);
   fclose (fw);
@@ -77,7 +80,7 @@ int readlines (char *lineptr[], int nlines, FILE *fr) {
       //line[iLine -1] = '\0'; // delete newline
       strcpy (p, line);
       lineptr[nlines++] = p;
-      //printf ("\n ATUL: lineptr = %s, p=%s, line=%s\n", lineptr[nlines-1], p, line);
+      //printf ("\n ATUL: \nlineptr = %s, \np       = %s, \nline    = %s, \nnlines  = %d\n", lineptr[nlines-1], p, line, nlines);
     } // else
   }
   return nlines;
@@ -97,17 +100,44 @@ int getLine (char s[], int lim, FILE *fr) {
     s[i] = '\0';
     i++;
   }
+  //printf ("\ns[i] = %s, i=%d\n", s, i);
   return i;
 }
 
+/*<<< bugy code >>>*/
+/* qsort: sort v[left]....v[right] into increasing order */
 void qsort_n (char *lineptr[], int left, int right) {
+  //printf ("\nATUL: lineptr = %c lef=%d, right=%d\n", lineptr[0][0], left, right);
+  int i, last;
+  void swap (char *v[], int i, int j);
 
-  printf ("\nATUL: lineptr = %c lef=%d, right=%d\n", lineptr[0][0], left, right);
+  if (left == right)      // do nothing if array contains fewer than two elements.
+    return;
+  swap(lineptr, left, (left+right)/2);
+    last = left;
 
+  for(i = left + 1; i <= right; i++)
+    if(strcmp(lineptr[i], lineptr[left]) < 0)
+       swap(lineptr, ++last, i);
+
+  swap(lineptr, left, last);
+  qsort_n(lineptr, left, last-1);
+  qsort_n(lineptr, last+1, right);
 }
 
-void writelines (char *lineptr[], int nlines) {
+void swap(char *v[], int i, int j)
+{
+     char *temp;
 
+     temp = v[i];
+     v[i] = v[j];
+     v[j] = temp;
+}
+
+/* writelines: write output lines*/
+void writelines (char *lineptr[], int nlines) {
+  while (nlines-- > 0)
+    printf (" %s\n", *lineptr++);
 }
 
 
