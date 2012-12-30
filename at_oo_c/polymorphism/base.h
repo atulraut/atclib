9a117b2cf94dd1d25248655e2dd7bd3ac2233945
Atul Raut <rauji.raut@gmail.com>
2012-12-30 23:07:57 +0530

    OOP using c --> polymorphism

---
+++ at_oo_c/polymorphism/base.h
@@ -0,0 +1,20 @@
+/***
+* Aim : Implement Polymorphism using C.
+* Date : Thursday, December 20 2012, 11:50 PM
+* File : base.h
+*/
+
+#ifndef BASE_H_
+#define BASE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "public_defs.h"
+#include "plug-01.h"
+
+//ats_poll_table_type ats_poll_list[1] = {&plug_1_handle};
+
+
+#endif
+
+
