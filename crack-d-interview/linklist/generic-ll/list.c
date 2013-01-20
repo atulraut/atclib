6c4abd55c53b0164ae8d0fa4af8b58e41d2321fd
Atul Raut <rauji.raut@gmail.com>
2013-01-21 00:28:43 +0530

    Generic Linklist, reading writing using File Operation.

--- crack-d-interview/linklist/generic-ll/list.c
+++ crack-d-interview/linklist/generic-ll/list.c
@@ -1,54 +1,74 @@
+/*
+* Aim  : WAP to implement Generic Linked list.
+* Date : Saturday, January 5th & 6th 2013 12:00 PM
+* By   : Atul R. Raut
+* File : list.c implementation of generic linked list.
+*
+***/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "list.h"
 
-list * list_init (list *_list) {	
+list * list_init (list *_list) {
     _list = (list*)malloc(sizeof(list));
     if (_list == NULL)
-    printf ("\nFailed to allocate memory.\n");
+      printf ("\nFailed to allocate memory.\n");
     else {
-_list->NumEl = 0;
-_list->pFirst = NULL;
-_list->pLast  = NULL;
-_list->pCurr  = NULL;
+            _list->NumEl = 0;
+            _list->pFirst = NULL;
+            _list->pLast  = NULL;
+            _list->pCurr  = NULL;
     }
     return (_list);
 }
 
-list_node * create_node (void *_data, size_t len) {
-list_node *newNode = NULL;
-newNode = (list_node*) malloc (sizeof (list_node));
-if (len > 0) {
-newNode->data = (char *) malloc (len);	
-if (newNode->data == NULL) {
-free(newNode);
-return NULL;
-} else {
-newNode->data = (char*) _data;
-}
-}
-return newNode;
+list_node * create_node (void *_data, int len) {
+    list_node *newNode = NULL;
+    newNode = (list_node*) malloc (sizeof (list_node));
+    if (len > 0) {
+       newNode->data = (char *) malloc (len+1);
+       if (newNode->data == NULL) {
+          free(newNode);
+          return NULL;
+       } else {
+         newNode->data = (char*) _data;
+       }
+    }
+    return newNode;
 }
 
-void list_add (list *_list, void *_data, size_t len) {
-list_node *newNode = NULL;
-newNode = create_node (_data, len);
-if (NULL == newNode)
-printf ("\nFailed to allocate memory");
-else {
-_list->NumEl += 1;
-if(_list->NumEl == 1) {
-newNode->next = newNode->prev= NULL;
-_list->pFirst = _list->pLast = _list->pCurr = newNode;
+void list_add (list *_list, void *_data, int len) {
+    list_node *newnode = NULL;
+    newnode = create_node (_data, len);
+    if (NULL == newnode)
+        printf ("\nfailed to allocate memory");
+    else {
+         _list->NumEl += 1;
+         if(_list->NumEl == 1) {
+            _list->pFirst = newnode;
+            newnode->next = NULL;
+            newnode->prev = NULL;
+         } else {
+           newnode->prev = _list->pLast;
+           newnode->next = NULL;
+           _list->pLast->next = newnode;
 }
-}
+_list->pLast = newnode;
+    } // first else end
 }
 
 void list_print (list *_list) {
-	int i;
-	printf ("\nHi Print = %d\n", _list->NumEl);
-	for(i=0; i<_list->NumEl; i++) {
-		printf("\nAT::-->%d", (char *)_list->pFirst->data);
-	}
+    int i;
+    list_node *temp = NULL;
+    temp = _list->pFirst;
+    i = 1;
+    while (temp != _list->pLast->next) {
+         printf("-->[%s]", (char *)temp->data);
+         fputs((char *)temp->data, fw);
+         fputc('\n', fw);
+         temp = temp->next;
+         i++;
+    }
+    printf ("\n\n");
 }
-
