3ec4c2c5cc3e0705d9af9ef553316a19049665bc
Atul Raut <rauji.raut@gmail.com>
2014-06-20 04:28:59 +0000

    Calling at_strstr from main routine.

--- temp/string/substr.c
+++ temp/string/substr.c
@@ -3,11 +3,19 @@
 * Date : 3-07-2013
 * Author : Atul Ramesh Raut
 ***/
+#include <stdio.h>
+#include <stdlib.h>
 
 char* at_strstr(const char *str, const char *substr);
 
 int main () {
 
+  char *ptr;
+  char *gtr;
+  ptr = strstr("This is Great", "Great");
+  printf ("ptr = %s\n", ptr);
+  gtr = at_strstr("This is Great", "Grea");
+  printf ("gtr = %s\n", gtr);
   return 0;
 }
 
@@ -25,9 +33,9 @@ char * at_strstr (const char *str, const char *substr) {
     while (1) {
       if (*tempsubstr == '\0')
         return str;
-      if (*temp != *tempsubstr)
+      if (*tempstr != *tempsubstr)
         break;
-      temp++;
+      tempstr++;
       tempsubstr++;
     }  
     tempsubstr = substr;
