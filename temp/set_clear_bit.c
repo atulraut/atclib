7191d777a381ade22c86f5f09dec22f12e94a8ba
Atul Ramesh Raut <atulraut17@gmail.com>
2015-09-21 22:59:54 -0700

    Create set_clear_bit.c
    Added function set bit, clear bit & function which returns position of first 1 from right left in binary representation of an Integer.

---
+++ temp/set_clear_bit.c
@@ -0,0 +1,51 @@
+/*
+* Author  : Atul Raut
+* Date    : 21 September 2015
+* Place   : San Diego
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+
+int setBit (int n, int k);
+int clearBit (int n, int k);
+int toggleBit (int n, int k);
+unsigned int getFirstSetBitPos(int n);
+
+int main() {
+
+    printf("setBit   = %d \n", setBit (9, 5) );
+    printf("clearBit = %d \n", clearBit (12, 4) );
+    printf("toggleBit = %d \n", toggleBit (9, 5) );
+ 
+    printf("getFirstSetBitPos = %u \n\n", getFirstSetBitPos(18)); // O/P = 2 
+    return 0;
+}
+
+int setBit (int n, int k) {
+    if (k<=0)
+        return 0;
+    return (n | (1<<(k-1)));
+}
+
+int clearBit (int n, int k) {
+    if (k<=0)
+        return 0;
+    return (n & ~(1<<(k-1)));
+}
+// Recheck it 
+int toggleBit (int n, int k) {
+    if (k<=0)
+        return 0;
+    return (n ^ (1<<(k-1)));
+} 
+
+/*
+ * Write a one line C function to return position of first 1 from right to left, 
+ * in binary representation of an Integer.
+ */
+unsigned int getFirstSetBitPos(int n)
+{
+   return log2(n&-n)+1;
+}
+ 
