56bc337cc6108070150fe324b73aad41e174051e
Atul Raut <rauji.raut@gmail.com>
2016-08-08 22:11:48 -0700

    routine to get square without using  * or carrot sign

--- crack-d-interview/sum2intno+.c
+++ crack-d-interview/sum2intno+.c
@@ -43,3 +43,12 @@ int sumRecursive(int a, int b) {
   else if (b > 0) return sumRecursive(++a, --b);
   else return sumRecursive(--a, ++b);
 }
+
+/*How can we get square of a number without using * or carrot sign.*/
+int square(int num){
+	
+	int i,sum=0,j=1;
+	for(i=0;i<num;i++,j+=2)
+		sum+=j;
+	return sum;
+}
