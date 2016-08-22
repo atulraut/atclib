eae825f556879f040d35640892f83caab380a40a
Atul Raut <rauji.raut@gmail.com>
2016-08-21 17:24:32 -0700

    generic link list programs

---
+++ crack-d-interview/linklist/singly_void/list.h
@@ -0,0 +1,29 @@
+#ifndef ADD_H_LIST
+#define ADD_H_LIST
+
+typedef struct list_n {
+    int data;
+    struct list_n *next;
+} LIST_NODE;
+
+typedef struct list_t {
+  struct list_t *head;
+} LIST;
+
+int counter;
+LIST * m_Init ();
+LIST_NODE* m_CreateNodeL (int);
+void m_Addfirst(int, void*);
+void m_Addlast(int, void *);
+void m_Delfirst();
+void m_Display(void *);
+void m_Dellast(void *);
+void m_Freelist(void *);
+void m_Revlist(void *); 
+/*
+void m_Insert(int,int);
+void m_Deletenode(int);
+void m_Printrev ();
+void m_RevRec();
+*/
+#endif
