6f956a01e0ecbb7f7c18587b400ff9520055ce0f
Atul Raut <rauji.raut@gmail.com>
2016-09-14 09:40:59 -0700

    reverseString

--- happycodings/testString.c
+++ happycodings/testString.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-
 char* atReverse (char s[]);
 
 int main () {
-
   char r[4] = "Atul";
   printf ("\nAT r = %s\n", r);
   char *p;
@@ -12,16 +10,11 @@ int main () {
   printf ("\n Reverser = %s", p); 
 }
 
-
-
 /* atReverse : that reverses the charcater string s */
 char* atReverse (char s[]) {
   int i, j;
-  char *copy;
-  copy = NULL;
-  copy = s;
-  i = 0;
-  j = 0;
+  char *copy = s;
+  i = 0; j = 0;
   while (s[i] != '\0') 
     i++;
   i--;
@@ -31,8 +24,17 @@ char* atReverse (char s[]) {
     j++;
     i--;
   }
-  //j++;
   copy[j] = '\0';
   printf ("\nAT>>>>>>>>>>>> New Copy = %s, J=%d", copy, j);
   return copy;
+}
+
+void reverseString(char s[]) {
+    int length = strlen(s) ;
+    int c, i, j;
+    for (i = 0, j = length - 1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
 }
