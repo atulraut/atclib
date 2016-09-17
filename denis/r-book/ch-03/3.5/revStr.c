14e7eb1d0dd1842573b71a027ef716547ae3664f
Atul Ramesh Raut <atulraut17@gmail.com>
2016-09-17 00:14:36 -0700

    recursive String Reverse & reverse word

--- denis/r-book/ch-03/3.5/revStr.c
+++ denis/r-book/ch-03/3.5/revStr.c
@@ -11,11 +11,15 @@
 
 void revStr (char *);
 void reverseString(char s[]);
+void  otherWay();
+void rev(char *l,char *r);
+void reverseRecursive(char str1[], int index, int size);
 
 int main () {
   printf ("\nHello world .. !! \n");
   char arr[10] = "Atul";
   revStr (arr);
+  otherWay();
   return EXIT_SUCCESS; 
 }
 
@@ -26,9 +30,10 @@ void reverseString(char s[]) {
         c = s[i];
         s[i] = s[j];
         s[j] = c;
+    }
 }
     
-void revStr (char *s) {
+void revStr (char *str) {
     int j;
     char *end = str;
     j = strlen (str)-1;
@@ -49,4 +54,44 @@ void revStr (char *s) {
         } while (j != 0);
     }
     printf ("\n Reverse String = %s\n", end);        
+}
+
+void reverseRecursive(char str1[], int index, int size) {
+    char temp;
+    temp = str1[index];
+    str1[index] = str1[size - index];
+    str1[size - index] = temp;
+    if (index == size / 2) {
+        return;
+    }
+    reverseRecursive(str1, index + 1, size);
+}
+
+void rev(char *l,char *r) {
+  char t;
+  while(l<r) {
+    t = *l;
+    *l++ = *r;
+    *r-- = t;
+  }
+}
+
+void otherWay () {
+  char buf[] = "Atul R Raut";
+  char *end, *x, *y;
+  for(end=buf; *end; end++); // reach enf of string first.
+  rev(buf,end-1);
+  printf("ANTU buf = %p, buf = %s end = %s \n",buf, buf, end);
+  // Now swap each word within sentence...
+  x = buf-1;
+  y = buf;
+  while(x++ < end) {
+    if(*x == '\0' || *x == ' ') {
+      rev(y,x-1);
+      y = x+1;
+    }
+  } 
+  printf("Output = %s\n",buf);
+  reverseRecursive(buf, 0, (strlen(buf)-1));
+  printf("Output = %s\n",buf);
 }
