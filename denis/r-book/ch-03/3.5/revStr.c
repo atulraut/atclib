6c0017de1084800b1e76b798aefb340355a0e642
Atul Raut <rauji.raut@gmail.com>
2016-09-17 00:01:06 -0700

    Fix Reverse String Issues

--- denis/r-book/ch-03/3.5/revStr.c
+++ denis/r-book/ch-03/3.5/revStr.c
@@ -2,6 +2,7 @@
 * Aim : Reverse String.
 * Author : Atul Ramesh Raut
 * Date   : March 30, 2012 08:47PM
+* Date   : Sept  17, 2016 12:00AM
 *
 ***/
 
@@ -9,6 +10,7 @@
 #include <stdlib.h>
 
 void revStr (char *);
+void reverseString(char s[]);
 
 int main () {
   printf ("\nHello world .. !! \n");
@@ -17,20 +19,34 @@ int main () {
   return EXIT_SUCCESS; 
 }
 
+void reverseString(char s[]) {
+    int length = strlen(s) ;
+    int c, i, j;
+    for (i = 0, j = length - 1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+}
+    
 void revStr (char *s) {
-        char *p = NULL;
-        char temp;
-        p = s;               
-        while (*p != '\0')
-                p++;
-	p--;
-        while (s < p) {
-                temp = *s;
-                *s = *p;
-                *p = temp;                
-                s++;      
-		p--;
+    int j;
+    char *end = str;
+    j = strlen (str)-1;
+    char temp;
+    if (str)  { 
+        while (*end != '\0')
+            end++;
+        --end;
+        while (str < end) {
+            temp = *str;
+            *str++ = *end;
+            *end-- = temp;
+            j--;
         }
-	p--;
-	printf ("\n Reverse String = %s\n", p);        
+        do {
+            --end;
+            j--;
+        } while (j != 0);
+    }
+    printf ("\n Reverse String = %s\n", end);        
 }
