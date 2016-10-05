e81caf395d6cce31698173a9e83f4d9b8193e0e3
Atul Raut <rauji.raut@gmail.com>
2016-10-05 13:28:49 -0700

    Select a country where the probability of being selected was  proportional to its population
    Select a country where the probability of being selected was  proportional to its population

---
+++ crack-d-interview/google/get_country.c
@@ -0,0 +1,44 @@
+/*
+ * Q1: How would you select a country where the probability of being selected was
+ * proportional to its population?
+ * by Google
+ * Oct 3, 2016
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+
+char *get_country (char *countries[], int *population, int size);
+
+int main () {
+  int i, size;
+  char *ret;
+  char *countries[] = {"Taiwan", "Koria", "Mexico", "india", "USA", "China"};
+  int population[] = {2, 3, 4, 10, 11, 20/*90, 95, 20*/};
+  size = sizeof(countries)/sizeof (countries[0]);
+  for (i=0; i<6; i++) {
+    ret = get_country (countries, population, size);
+    printf ("ret = %s\n", ret);
+  }
+  return 0;
+}
+
+char *get_country (char *countries[], int *population, int sz) {
+  int i, fst, mid, key, end;
+  int *count = (int *)malloc (sizeof(sz));
+  count[0] = population[0];
+  for(i=1; i<sz; i++)
+    count[i] = count[i-1] + population[i];
+  key = 1 + rand() % count[sz-1];
+  printf ("key=%d, cnt=%d \n", key, count[sz-1]);
+  fst = 0;
+  end = sz-1;
+  while (fst <= end) {
+    mid = fst + (end-fst)/2;
+    if (count[mid] < key)
+      fst = mid + 1;
+    else
+      end = mid - 1;
+  } // end while
+  return countries[fst];
+}
