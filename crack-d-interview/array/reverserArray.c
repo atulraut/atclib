a2d4cc08f57dafa717c862da51369e5168dffd4a
Atul Raut <rauji.raut@gmail.com>
2017-06-03 11:18:36 -0700

    Update reverserArray.c

--- crack-d-interview/array/reverserArray.c
+++ crack-d-interview/array/reverserArray.c
@@ -1,35 +1,50 @@
 /*
- * Reverse an array without using iteration. 
+ * Implement a function rotateArray(vector<int> arr, int r) which rotates the array by r places. 
+ *  Eg 1 2 3 4 5 on being rotated by 2 gives 4 5 1 2 3.
  * San Diego
  * Date : 11 March 2017
 */
 #include <stdio.h>
 
-void reverseArray (int *arr, int fst, int sz);
+void rotateArray(int *, int, int);
+void reverseArr(int *, int, int);
+void reverseArray (int *, int, int);
 
 int main() {
-    int i = 0, fst=0, sz=0;
-    int arr[5] = {5,4,3,2,1};
-    sz = sizeof(arr)/sizeof(arr[0]);
+    int i;
+    int arr[5] = {1, 2, 3, 4, 5};
+    int sz = sizeof(arr)/sizeof(arr[0]);
+    /*O/P: 4 5 1 2 3. */
+    int ret;
+    rotateArray(arr, sz, 3);
+//    int ret = pwr(2, 2);
+    printf("ANS = %d !\n", sz);
     for (i=0; i<5; i++) {
-        printf("->[%d]", arr[i] );
+        printf ("-->[%d]\t", arr[i]);
     }
-    printf("\n");
-    reverseArray (arr, fst, sz);
-    for (i=0; i<5; i++) {
-        printf("->[%d]", arr[i] );
-    }
-    printf("\n");
+    printf ("\n");
     return 0;
 }
 
-void reverseArray (int *arr, int fst, int sz) {
-    int temp = 0;
-    printf ("arr[%d], arr[%d] \n", arr[fst], arr[sz]);
-    if (fst > sz)
-        return;
-    temp = arr[fst];
-    arr[fst] = arr[sz-1];
-    arr[sz-1] = temp;
-    reverseArray (arr, ++fst, --sz);
+int pwr(num, power) { 
+    if(power==0) return 1; 
+    if(power==1) return num; 
+    return( num * raise(num, power-1) ); 
+}
+
+void rotateArray(int *in, int arrsz, int rotate){ 
+    int i =0, j = arrsz-1;; 
+    reverseArr(in, i, j); 
+    reverseArr(in, 0, rotate-1); 
+    reverseArr(in, rotate, j); 
+} 
+
+void reverseArr(int *in, int arrsz, int rotate) { 
+    int i =arrsz,j = rotate; 
+    while (i <= j) { 
+        int tmp = in[i]; 
+        in[i] = in[j]; 
+        in[j] = tmp; 
+        i++; j--; 
+    } 
 }
