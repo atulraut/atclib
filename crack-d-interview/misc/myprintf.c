2016a90ac57850deb494537cbdfbdccba26d2d81
Atul Raut <atulraut17@gmail.com>
2019-08-07 07:10:54 -0700

    crack-d-interview: Add own printf routine.
    Add own printf routine.
    
    Signed-off-by: Atul Raut <atulraut17@gmail.com>

--- crack-d-interview/misc/myprintf.c
+++ crack-d-interview/misc/myprintf.c
@@ -1,67 +1,74 @@
 /*
-* copyright : with WWW.FIRMCODES.COM 
+* copyright : with WWW.FIRMCODES.COM
+* atclib: Added comments, so that code is more readable.
 */
-#include<stdio.h> 
-#include<stdarg.h>                      
+#include<stdio.h>
+#include<stdarg.h>
 
-void Myprintf(char *,...);              //Our printf function
+void Myprintf(char *,...);              /* Our printf function */
 char* convert(unsigned int, int);       //Convert integer number into octal, hex, etc.
 
-int main() { 
-  Myprintf(" WWW.FIRMCODES.COM \n %d", 9); 
+int main() {
+  Myprintf(" WWW.FIRMCODES.COM \n %d", 9);
   return 0;
-} 
+}
 
-void Myprintf(char* format,...) { 
-  char *traverse; 
-  unsigned int i; 
-  char *s; 
-  //Module 1: Initializing Myprintf's arguments 
-  va_list arg; 
-  va_start(arg, format); 
-  for(traverse = format; *traverse != '\0'; traverse++)    { 
-      while( *traverse != '%' ) { 
+void Myprintf(char* format,...) {
+  char *traverse;
+  unsigned int i;
+  char *s;
+  /* Module 1: Initializing Myprintf's arguments */
+  /* va_list: It is a Data Type for accessing data from
+   * variable argument list.
+   **/
+  va_list arg; /* This define Data Type */
+  /* va_start: It is a micro that initialize the
+   * variable arg. list to the 1st argument.
+   **/
+  va_start(arg, format);
+  for(traverse = format; *traverse != '\0'; traverse++)    {
+      while( *traverse != '%' ) {
 	  putchar(*traverse);
-	  traverse++; 
-      } 
-      traverse++; 
+	  traverse++;
+      }
+      traverse++;
       //Module 2: Fetching and executing arguments
-      switch(*traverse) { 
+      switch(*traverse) {
 	case 'c' : i = va_arg(arg,int);     //Fetch char argument
 	  putchar(i);
-	  break; 
+	  break;
 	case 'd' : i = va_arg(arg,int);         //Fetch Decimal/Integer argument
-	  if(i<0) { 
+	  if(i<0) {
 	      i = -i;
-	      putchar('-'); 
-	   } 
+	      putchar('-');
+	   }
 	  puts(convert(i,10));
-	  break; 
+	  break;
 	case 'o': i = va_arg(arg,unsigned int); //Fetch Octal representation
 	  puts(convert(i,8));
-	  break; 
+	  break;
 	case 's': s = va_arg(arg,char *);       //Fetch string
-	  puts(s); 
-	  break; 
+	  puts(s);
+	  break;
 	case 'x': i = va_arg(arg,unsigned int); //Fetch Hexadecimal representation
 	  puts(convert(i,16));
-	  break; 
-        }   
-    } 
+	  break;
+        }
+    }
 
   //Module 3: Closing argument list to necessary clean-up
-  va_end(arg); 
-} 
+  va_end(arg)
+}
 
-char *convert(unsigned int num, int base) { 
+char *convert(unsigned int num, int base) {
   static char Representation[]= "0123456789ABCDEF";
-  static char buffer[50]; 
-  char *ptr; 
-  ptr = &buffer[49]; 
-  *ptr = '\0'; 
-  do { 
-      *--ptr = Representation[num%base]; 
-      num /= base; 
-    } while(num != 0); 
-  return(ptr); 
+  static char buffer[50];
+  char *ptr;
+  ptr = &buffer[49];
+  *ptr = '\0';
+  do {
+      *--ptr = Representation[num%base];
+      num /= base;
+    } while(num != 0);
+  return(ptr);
 }
