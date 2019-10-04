2babc698b207e447ff1fd20ce8094408644c6b29
Atul Raut <rauji.raut@gmail.com>
2019-10-04 07:42:12 -0700

    Add OOO code in C
    used reference ooo code in C to rewrite Queue
    implementation.
    
    Signed-off-by: Atul Raut <atulraut17@gmail.com>

--- crack-d-interview/function_pointer/array_of_pointer.c
+++ crack-d-interview/function_pointer/array_of_pointer.c
@@ -1,14 +1,48 @@
 /*
 	21st-century-c-o-reilly-ben-klemens.pdf
 	Example 66.7_Page Num - 112
-	Array of Pointers
+	1. Array of Pointers
+	2. Pointer to an Array : https://www.geeksforgeeks.org/pointer-array-array-pointer/
 */
 #include <stdio.h>
+
+void  test_array_of_pointers() {
+  int *ptr[5];
+  int arr[5] = {2,3,4,5,6};
+  int i;
+
+  for(i=0; i<5; i++)
+    ptr[i] = &arr[i];
+
+  for(i=0; i<5; i++)
+    printf ("--> i=%d\n", *ptr[i]);
+}
+
+void test_pointers_to_an_array() {
+    int arr[] = { 3, 5, 6, 7, 9 };
+    int *p = arr;
+    int (*ptr)[5] = &arr;
+
+    printf("p = %p, ptr = %p\n", p, ptr);
+    printf("*p = %d, *ptr = %p\n", *p, *ptr);
+
+    printf("sizeof(p) = %lu, sizeof(*p) = %lu\n", sizeof(p), sizeof(*p));
+    printf("sizeof(ptr) = %lu, sizeof(*ptr) = %lu\n", sizeof(ptr), sizeof(*ptr));
+}
+/*
+  o/p:
+  p = 0x7ffdaba606d0, ptr = 0x7ffdaba606d0
+  *p = 3, *ptr = 0x7ffdaba606d0
+  sizeof(p) = 8, sizeof(*p) = 4
+  sizeof(ptr) = 8, sizeof(*ptr) = 20
+*/
 
 int main () {
   char *list[] = {"Atul", "Ramesh", "Raut", NULL};
   for(char **p = list; *p != NULL; p++) {
     printf ("--> %s \t", p[0]);
   }
+  test_array_of_pointers();
+  test_pointers_to_an_array();
   printf ("\n\n");
 }
