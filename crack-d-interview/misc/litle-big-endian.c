219e069caf2884749c8a4e88d7e7a3fe21f71943
Atul Raut <rauji.raut@gmail.com>
2019-10-10 22:42:57 -0700

    Update Little Endian Program
    Signed-off-by: Atul Raut <atulraut17@gmail.com>

---
+++ crack-d-interview/misc/litle-big-endian.c
@@ -0,0 +1,19 @@
+/*
+ * https://www.geeksforgeeks.org/little-and-big-endian-mystery/
+ * Little and big endian are two ways of storing multibyte data-types ( int, float, etc).
+ * In little endian machines, last byte of binary representation of the multibyte
+ * data-type is stored first. On the other hand, in big endian machines,
+ * first byte of binary representation of the multibyte data-type is stored first.
+*/
+
+#include <stdio.h>
+
+int main() {
+  unsigned int i = 1;
+  char *c = (char*)&i;
+  if (*c)
+    printf ("Little endian \n");
+  else
+    printf("Big endian \n");
+  return 0;
+}
