7343ded4ff6f6852c00cba948eee273ecd8d33fc
Atul Raut <atulraut17@gmail.com>
2019-10-17 22:51:20 -0700

    leetcode: Add sort array by parity
    Add misc code.
    
    Signed-off-by: Atul Raut <atulraut17@gmail.com>

--- crack-d-interview/misc/arg_2_main.c
+++ crack-d-interview/misc/arg_2_main.c
@@ -7,8 +7,8 @@
   argv is array of character pointer
 */
 int main (int argc, char *argv[]) {
-	int i;
-	for(i=0; i<argc; i++) {
-		printf ("--> %s \n", argv[i]);
-	}
+  int i;
+  for(i=0; i<argc; i++) {
+    printf ("--> %s \n", argv[i]);
+  }
 }
--- crack-d-interview/misc/sz_mem_malloc_ptr.c
+++ crack-d-interview/misc/sz_mem_malloc_ptr.c
@@ -1,15 +1,18 @@
 /*
-  copy paste from : https://stackoverflow.com/questions/7202069/how-can-i-know-the-allocated-memory-size-of-pointer-variable-in-c
+  copy paste from :o https://stackoverflow.com/questions/7202069/how-can-i-know-the-allocated-memory-size-of-pointer-variable-in-c
 */
 #include <stdlib.h>
 #include <stdio.h>
 
 void * my_malloc(size_t s) {
-  printf ("sizeof(size_t) = %d \n", (sizeof(size_t) + s));
+  printf ("sizeof(size_t) = %ld s = %ld \n", (sizeof(size_t)), (sizeof(size_t) + s));
+
   size_t* ret = malloc(sizeof(size_t) + s);
-  printf ("[my_malloc] sz ret = %p s = %d sp = %p\n", ret, s, s);
+
+  printf ("[my_malloc] sz ret = %p s = %ld \n", ret, s);
+
   *ret = s;
-  printf ("[my_malloc] sz     ret    = %p \n", ret);
+  printf ("[my_malloc] sz     ret    = %p *ret= %ld \n", ret, *ret);
   printf ("[my_malloc] sz    &ret    = %p \n", &ret);
   printf ("[my_malloc] sz    &ret[0] = %p \n", &ret[0]);
   printf ("[my_malloc] sz    &ret[1] = %p \n", &ret[1]);
@@ -24,20 +27,22 @@ void my_free(void * ptr) {
 }
 
 size_t allocated_size(void * ptr) {
+
   printf ("[allocated_size] sz PTR-ADR= %p \n", (ptr));
-  printf ("[allocated_size] sz szPTR= %p \n", ((size_t*)ptr)[-1]);
+  printf ("[allocated_size] sz szPTR= %zu \n", ((size_t*)ptr)[-1]);
+
   return ((size_t*)ptr)[-1];
 }
 
 int main(int argc, const char ** argv) {
   int* array = NULL;
+
   array = my_malloc(sizeof(int) * 3); /* int sz = 4 bytes here*/
-  printf ("[main] sz = %p \n", array);
-  printf ("[main] og = %u \n", allocated_size(array));
-  printf("%u\n", allocated_size(array));
+
+  printf ("--> [main] Address = %p \n", array);
+  printf ("[main] og = %ld \n", allocated_size(array));
+
   my_free(array);
-  array = malloc(sizeof(int) * 3);
-  printf("[main] %u\n", allocated_size(array));
-  my_free(array);
+
   return 0;
 }
