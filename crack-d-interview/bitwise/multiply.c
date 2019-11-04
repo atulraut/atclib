0a6546fafa2e9284b8a8eee94596511b326e0ebf
Atul Raut <rauji.raut@gmail.com>
2019-11-04 07:39:57 -0800

    crack-d-interview: Add multiply program
    Add routine multiply without using multiply
    operator.
    
    Signed-off-by: Atul Raut <atulraut17@gmail.com>

---
+++ crack-d-interview/bitwise/multiply.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int multiply(int x, int y) {        
+        if (y == 0)
+                return 0;
+        
+        if(y > 0) {
+                return (x+multiply(x, y-1));
+        }
+        
+        if(y < 0) {
+                return -multiply(x, -y);
+        }
+}
+
+int main () {
+        printf ("multiplication --> %d\n\n", multiply(5, -11));
+}
