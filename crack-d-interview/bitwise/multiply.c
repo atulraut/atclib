9627013ac1b4e09ff574dda32ea03abe2950da33
Atul Raut <atulraut17@gmail.com>
2019-11-06 09:26:44 -0800

    Update insertion sort
    Signed-off-by: Atul Raut <atulraut17@gmail.com>

--- crack-d-interview/bitwise/multiply.c
+++ crack-d-interview/bitwise/multiply.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int multiply(int x, int y) {        
+int multiply(int x, int y) {
         if (y == 0)
                 return 0;
-        
+
         if(y > 0) {
                 return (x+multiply(x, y-1));
         }
-        
+
         if(y < 0) {
                 return -multiply(x, -y);
         }
