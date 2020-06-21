56ea59b009f2301491d18e253a762a16bd55eb38
Atul Raut <rauji.raut@gmail.com>
2020-06-20 18:55:44 -0700

    LeetCode: Add heap sorting
    advance_c/search_array_index_bin_search.c
    crack-d-interview/tree_graph/simple_tree/list.h
    crack-d-interview/tree_graph/simple_tree/tree.c
    leetCode/array/random_pick_with_weight.c
    leetCode/array/sorting/heap_sort/lastStoneWeight.c
    
    Signed-off-by: Atul Raut <rauji.raut@gmail.com>

--- advance_c/search_array_index_bin_search.c
+++ advance_c/search_array_index_bin_search.c
@@ -1,10 +1,10 @@
-+/*
-+* Author : Atul R. Raut
-+* Date   : 27 Sept 2015
-+* Place  : San Diego
-+* Do Check : http://geeksquiz.com/binary-search/
-+* Question: Search a given number in an array and return its index using binary search tree.
-+*/
+/***
+ * Author : Atul R. Raut
+ * Date   : 27 Sept 2015
+ * Place  : San Diego
+ * Do Check : http://geeksquiz.com/binary-search/
+ * Question: Search a given number in an array and return its * index using binary search tree.
+ */
 
 #include <stdio.h>
  
@@ -30,7 +30,7 @@ int main(void) {
    int arr[] = {2, 3, 4, 10, 40, 89, 96};
    int high = sizeof(arr)/ sizeof(arr[0]);
 //   printf ("sizeof-arr = %d, arr[0] = %d\n", sizeof(arr), sizeof(arr[0]));
-   int key = 89;
+   int key = 4;
    int result = binarySearch(arr, 0, high-1, key);
    (result == -1)? printf("Element is not present in array")
                  : printf("Element is present at index %d", result);
