6372cba0a34030c25179be8242cd9b83f252d3a6
Atul Raut <rauji.raut@gmail.com>
2020-09-04 18:58:55 -0700

    LeetCode : Add Trie Algo
    Add segfault problem
    Add removing duplicate entries in Array
    
    Signed-off-by: Atul Raut <rauji.raut@gmail.com>

--- crack-d-interview/array/rm_duplicate_arr.c
+++ crack-d-interview/array/rm_duplicate_arr.c
@@ -1,24 +1,52 @@
 /*
 * remove duplicates elements from array
 */
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+/***
+    LeetCode : Find All Duplicates in an Array
+    https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3414/
+    Note: The returned array must be malloced, assume caller calls free().
+ */
+int* findDuplicates(int* nums, int numsSize, int* returnSize){
+  int i, j;
+  printf ("\n[%s] Enter sz=%ld L=%d\n", __func__, sizeof(int [numsSize+1]), __LINE__);
+
+  int *rt=calloc(1,sizeof(int [numsSize+1]));
+
+  for(i=0; i<numsSize; i++){
+    printf ("[%s-A] nums[i]= %d rt[nums[i] = %d \n", __func__, nums[i], rt[nums[i]] );
+    rt[nums[i]] = (rt[nums[i]] == nums[i]) ? -1 : nums[i];
+    printf ("[%s-B] nums[i]= %d rt[nums[i]]=%d\n",__func__, nums[i], rt[nums[i]]);
+  }
+
+  for(i=1,j=1; *returnSize=j-1,i<numsSize+1; i++){
+    printf ("[%s-C] i=%d j=%d rt[i] = %d \n", __func__, i, j, rt[i] );
+    rt[i] == -1 ? rt[j++]=i : 0;
+  }
+
+  return rt+1;
+}
 
 int del_dup_array (int *arr, int size);
 void rev (int x);
-//int ar[] = {1,2,3,4,2,2, 6,7,8,9,11,12,24,45,45,46,33,36,32,7,87,79,65,43,21};
-int ar[] = {1,2,2,3,2,21};
+//int ar[] = {1, 2, 2, 3, 2, 21};
+int ar[] = {2, 4, 3, 2, 7};
 int del_dup_array (int arr[], int size) {
   int i, j, k;
   for (i = 0; i < size; i++) {
     for (j = i + 1; j < size;) {
       if (arr[j] == arr[i]) {
-	    for (k=j; k<size; k++) {
-	         if ((size-1) == k)
-	             arr[k+1] = 0;
-		 //	    	 printf ("i=%d, j=%d, k=%d, k=[%d], k+1=[%d] size = %d\n",i,j,k,  arr[k], arr[k + 1], size);
-	         arr[k] = arr[k + 1];
-	    }
-	    size--;
+for (k=j; k<size; k++) {
+  if ((size-1) == k)
+    arr[k+1] = 0;
+  // printf ("i=%d, j=%d, k=%d, k=[%d], k+1=[%d] size = %d\n",i,j,k,  arr[k], arr[k + 1], size);
+  arr[k] = arr[k + 1];
+}
+size--;
       } else
         j++;
     }
@@ -35,26 +63,19 @@ int main() {
   for (i = 0; i < size; i++) {
     printf("->[%d] ", ar[i]);
   }
+  int nums[] = {2, 4, 31, 2, 4};
+  int sz = sizeof(nums) / sizeof(nums[0]);
+  int retSz;
+  int*ret =  findDuplicates(nums, sz, &retSz);
   printf ("\n");
-  rev (2);
+  for (i = 0; i <retSz; i++) {
+    printf("Removing Duplicate & num is ->[%d] \n", ar[i]);
+  }
+
+  //rev (2);
   return (0);
 }
 
-void rev (int x) {
-  int i;
-  unsigned int y;
-  for (i=1; i<=10; i++) {
-    y = (i << 3) - (i << 0);
-    printf ("\n Mul by 07 = ->[%d] ", y);
-  }
-  for (i=1; i<=10; i++) {
-    y = (i << 3) + (i << 1);
-    printf ("\n Mul by 10 = ->[%d] ", y);
-  }
-  x=1;
-  printf ("\nx[0] = %d ", (x << 0));
-  printf ("\nx[1] = %d ", (x << 1));
-  printf ("\nx[2] = %d ", (x << 2));
-  printf ("\nx[3] = %d ", (x << 3));
-  printf ("\nx[4] = %d \n",(x << 4));
-}
+/***
+
+ */
