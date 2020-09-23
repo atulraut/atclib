9f216c31d2b15ce97748a90d5cad591dabc11223
Atul Raut <rauji.raut@gmail.com>
2020-09-23 15:49:06 -0700

    advance_c: Array - Pass & return 2D array from function.
    Leetcode : Add majority element program.
    
    Signed-off-by: Atul Raut <rauji.raut@gmail.com>

---
+++ crack-d-interview/misc/litle-big-endian_reverse.c
@@ -0,0 +1,35 @@
+/*
+ * https://www.geeksforgeeks.org/little-and-big-endian-mystery/
+ * Little and big endian are two ways of storing multibyte data-types ( int, float, etc).
+ * In little endian machines, last byte of binary representation of the multibyte
+ * data-type is stored first. On the other hand, in big endian machines,
+ * first byte of binary representation of the multibyte data-type is stored first.
+ * Little endian: Lower byte is stored on lower address.
+ * Big endian: Lower byte is stored on higher address.
+
+*/
+
+#include <stdio.h>
+
+/***
+ * Reverse Endianness :
+ */
+int reverse(int a) {
+  int x = a;
+  char *p = (char *) &x;
+  char *q = p + sizeof(a) - 1;
+  while (p < q) {
+    char t = *p;
+    *p = *q;
+    *q = t;
+    p++;
+    q--;
+  }
+  return x;
+}
+
+int main() {
+  unsigned int a = 0x12345678;
+  printf("Big endian  a = 0x%x\n", reverse(a));
+  return 0;
+}
