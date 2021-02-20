ced5c7adf2c4feeaad60ff863a4b4afb476d5b6a
Atul Raut <atul.raut@daimler.com>
2021-02-19 23:50:14 -0800

    function pointers : Add single line RTOS code
    Reorg few code
    
    Signed-off-by: Atul Raut <atul.raut@daimler.com>

--- advance_c/misc/crack-ivi/size_in_c.c
+++ advance_c/misc/crack-ivi/size_in_c.c
@@ -3,7 +3,13 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
-#include "../at_lib.h"
+
+#define debug(str,args...) printf("[%s] L=%d :"str"\n", __func__, __LINE__, ##args)
+
+
+void foo2() {
+  debug ("unsigned int=%lu long unsigned int =%lu ", sizeof(unsigned int), sizeof(long unsigned int) );
+}
 
 void foo1() {
   debug ("char=%lu short=%lu int=%lu long=%lu size_t=%lu ", sizeof(char), sizeof(short), sizeof(int), sizeof(long), sizeof(size_t) );
@@ -11,10 +17,12 @@ void foo1() {
 
 int main () {
   foo1();
+  foo2();
   return 0;
 }
 
 /**
    => ./a.out
    [foo1] L=9 :char=1 short=2 int=4 long=8 size_t=8
+   [foo2] L=10 :unsigned int=4 long unsigned int =8
 **/
