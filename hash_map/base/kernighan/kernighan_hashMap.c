dd3faad758abef4987c179e6bc37504b9ac6f1b1
Atul Raut <rauji.raut@gmail.com>
2021-03-15 21:40:30 -0700

    bitwise : Add bit array
    Letcode remove old hash code.
    
    Signed-off-by: Atul Raut <atul.raut@daimler.com>

--- hash_map/base/kernighan/kernighan_hashMap.c
+++ hash_map/base/kernighan/kernighan_hashMap.c
@@ -2,6 +2,7 @@
     HashMap : Algorithm by Kernighan's book.
     Date : 3/10/2021
     San Diego, CA
+    Ref: https://github.com/atulraut/the-practice-of-programming/blob/master/chapter-2/2.9-hash-tables/hash_tables.c
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,7 +24,7 @@ struct nameval {
 struct nameval *symtab[NHASH];
 
 /* Hash: compute hash value of string */
-unsigned int hash(char *str) {
+unsigned int get_hash(char *str) {
   unsigned int h = 0;
   unsigned char *p;
   h = 0;
@@ -37,11 +38,11 @@ unsigned int hash(char *str) {
 /* lookup: Find name in symtab, with optional create */
 struct nameval* lookup(char *name, int val, int isCreate) {
   struct nameval* sym = NULL;
-  int h = hash(name);
+  int h = get_hash(name);
 
   /* Lookup into hash Table */
   for (sym = symtab[h]; sym!= NULL; sym=sym->next)
-    if (strcmp(sym->name, name) == 0)
+    if (strncmp(sym->name, name, strlen(name)) == 0)
       return sym;
 
   if (isCreate) {
@@ -83,3 +84,21 @@ int main() {
 
   print_table();
 }
+
+/**
+   Exercise 2.14
+
+   Our hash function is an excellent general-purpose hash for strings.
+   Nonetheless, peculiar data might cause poor behaviour. Contruct a data set that
+   causes our hash function to perform badly. Is it easier to find a bad
+   set for different values of NHASH?
+
+   Answer: My experiment was to try out URL, since that was mentioned in the book
+   as possible problematic input. I tried with 8 URLs. In the beggining when NHASH
+   was way bigger that the number of inputs (500 > 8), there were no collisions.
+   However, when I set NHASH to something small collisions started popping.
+   For NHASH=8 (the same number of inputs) there were 4 collisions. Even for NHASH=16
+   (twice the number of inputs) there were 2 collisions, which was not good.
+   So my guess is that input of that sort would be problematic for this algorigthm.
+   And, of course, the smaller NHASH is, the bigger a possibility for collisi
+**/
--- linkedlist/leetcode/addtwonumbers/README.txt
+++ linkedlist/leetcode/addtwonumbers/README.txt
@@ -1,4 +1,4 @@
-Note : 
+Note :
 For few routines please assumed, USER has already created two linked list
 & accept them as input & work on it.
 If you directly called them, then you may hit seg fault.
