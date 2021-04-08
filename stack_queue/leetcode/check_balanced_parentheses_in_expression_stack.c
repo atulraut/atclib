3fc20e6309baec4dfe84b88870c846d03150f4b7
Atul Raut <rauji.raut@gmail.com>
2021-04-08 12:54:50 -0700

    LeetCode : 2 week April Challenge
    Add Inorder Successor in BST.
    Add Letter Combinations of a Phone Number.
    
    Signed-off-by: Atul Raut <rauji.raut@gmail.com>

--- stack_queue/leetcode/check_balanced_parentheses_in_expression_stack.c
+++ stack_queue/leetcode/check_balanced_parentheses_in_expression_stack.c
@@ -1,19 +1,19 @@
 /*
 	Check for balanced parentheses in an expression
-	Given an expression string exp , write a program to examine whether 
+	Given an expression string exp , write a program to examine whether
 	the pairs and the orders of “{“,”}”,”(“,”)”,”[“,”]” are correct in exp.
-	For example, the program should print true for exp = “[()]{}{[()()]()}” 
+	For example, the program should print true for exp = “[()]{}{[()()]()}”
 	and false for exp = “[(])”
 	Algorithm:
 	1) Declare a character stack S.
 	2) Now traverse the expression string exp.
-		a) If the current character is a starting bracket 
+		a) If the current character is a starting bracket
 		   (‘(‘ or ‘{‘ or ‘[‘) then push it to stack.
-		b) If the current character is a closing bracket 
-		   (‘)’ or ‘}’ or ‘]’) then pop from stack and if the popped 
-		   character is the matching starting bracket then fine else 
+		b) If the current character is a closing bracket
+		   (‘)’ or ‘}’ or ‘]’) then pop from stack and if the popped
+		   character is the matching starting bracket then fine else
 		   parenthesis are not balanced.
-	3) After complete traversal, if there is some starting bracket left 
+	3) After complete traversal, if there is some starting bracket left
 	   in stack then “not balanced”
 */
 
