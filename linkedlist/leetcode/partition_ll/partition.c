699b308bf108326125a4f5a1a437fedf719b4286
Atul Raut <rauji.raut@gmail.com>
2021-04-30 18:36:09 -0700

    LeetCode : Unique Paths using C.
    Add Unique Paths I
    Add Unique Paths II
    Add template file.
    
    Signed-off-by: Atul Raut <rauji.raut@gmail.com>

--- linkedlist/leetcode/partition_ll/partition.c
+++ linkedlist/leetcode/partition_ll/partition.c
@@ -1,10 +1,41 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
+/***
+    Medium: 86. Partition List
+    https://leetcode.com/problems/partition-list/
+
+    Given the head of a linked list and a value x, partition it such
+    that all nodes less than x come before nodes greater than or equal to x.
+
+    You should preserve the original relative order of the nodes in
+    each of the two partitions.
+
+    Input: head = [1,4,3,2,5,2], x = 3
+    Output: [1,2,2,4,3,5]
+
+    Input: head = [2,1], x = 2
+    Output: [1,2]
+
+    Constraints:
+    The number of nodes in the list is in the range [0, 200].
+    -100 <= Node.val <= 100
+    -200 <= x <= 200
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <limits.h>
+#include <string.h>  /* malloc */
+#include <stdbool.h>
+#include <math.h>
+#include <assert.h>
+#include <stdint.h> /* uint32_t */
+#include <unistd.h> /* sleep */
+
+#define debug(str,args...) printf("[%s] L=%d :"str"\n", __func__, __LINE__, ##args)
+
+struct ListNode {
+    int val;
+     struct ListNode *next;
+ };
 
 
 struct ListNode* partition(struct ListNode* head, int x) {
@@ -79,4 +110,8 @@ struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
 
   curr->next = curr->next->next;
   return new_head->next;
+}
+
+int main () {
+
 }
