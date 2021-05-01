7344e2d8372724a5330c67ef0cc716db4ed3ad53
Atul Raut <rauji.raut@gmail.com>
2021-04-30 18:36:09 -0700

    LeetCode : Unique Paths using C.
    Add Unique Paths I
    Add Unique Paths II
    Add template file.
    
    Signed-off-by: Atul Raut <rauji.raut@gmail.com>

---
+++ template.c
@@ -0,0 +1,24 @@
+/***
+
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <limits.h>
+#include <string.h>  /* malloc */
+#include <stdbool.h>
+#include <math.h>
+#include <assert.h>
+#include <stdint.h> /* uint32_t */
+#include <unistd.h> /* sleep */
+
+#define debug(str,args...) printf("[%s] L=%d :"str"\n", __func__, __LINE__, ##args)
+
+int main () {
+
+}
+
+/**
+
+ **/
