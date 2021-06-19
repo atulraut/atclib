a82f86b318d3059d33034c457043e5d15db1ddc7
Atul Raut <rauji.raut@gmail.com>
2021-06-19 11:11:22 -0700

    LeetCode: Add K Inverse Pairs Array using C
    Dynamic Programming:
    febonancci program using C.
    K Inverse Pairs Array using C.
    
    Signed-off-by: Atul Raut <rauji.raut@gmail.com>

--- advance_c/misc/crack-ivi/febonacci.c
+++ advance_c/misc/crack-ivi/febonacci.c
@@ -9,31 +9,45 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <limits.h>
+#include <string.h>  /* malloc */
+#include <stdbool.h>
+#include <math.h>
+#include <assert.h>
+#include <stdint.h> /* uint32_t */
+#include <unistd.h> /* sleep */
 
-int memoize(int N) {
-  int* cache = (int *)malloc(sizeof(int) * (N + 1));
+#define debug(str,args...) printf("[%s] L=%d :"str"\n", __func__, __LINE__, ##args)
+
+unsigned long long memoize(int N) {
+  unsigned long long ret = 0;
+  unsigned long long* cache = (unsigned long long *)malloc(sizeof(unsigned long long) * (N + 1));
 
   cache[1] = 1;
-
+  debug("cache[0]=%llu", cache[0]);
   for (int i = 2; i <= N; i++) {
     cache[i] = cache[i-1] + cache[i-2];
   }
 
-  return cache[N];
+  ret = cache[N];
+  free(cache);
+  return ret;
 }
 
 /* IVI : */
-int fib(int N) {
+unsigned long long fib(int N) {
   if (N <= 1) {
     return N;
   }
   return memoize(N);
 }
 
-
+static int cnt;
 int fab1 (int n) {
     int Res;
+    ++cnt;
     if (n==1 || n==2)
         return 1;
     Res = fab1(n-1) + fab1(n-2);
@@ -49,14 +63,20 @@ int main() {
   }
   printf ("o/p = %d \n", lastRes+Res);
 
-  int n = fib(10);
-  printf ("n = %d \n", n);
+  debug("Testing 50, cnt = %d", cnt);
+  unsigned long long n = fib(50);
+  printf ("n = %llu \n", n);
   return 0;
 }
 
 /*
-Output
- Res = 1  Res = 1  Res = 2  Res = 3  Res = 5
- o/p = 8
- n = 55
+  => ./a.out
+  Res = 1
+  Res = 1
+  Res = 2
+  Res = 3
+  Res = 5
+  o/p = 8
+  [main] L=65 :Testing 50!
+  n = 12586269025
 */
