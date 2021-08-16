bbc399e082992d73b3f70be0d7d528168aa13833
Atul Raut <rauji.raut@gmail.com>
2021-08-15 22:12:19 -0700

    Leetcode: Group Anagram
    Add String Grup Anangram using C.
    Add matrix size zero prog using C.
    Add rray of Doubled Pairs using C.
    
    Signed-off-by: Atul Raut <rauji.raut@gmail.com>

--- array/leetcode/group_anagram.c
+++ array/leetcode/group_anagram.c
@@ -1,3 +1,44 @@
+/***
+    Medium: Group Anagrams
+
+    Given an array of strings strs, group the anagrams together. You can
+    return the answer in any order.
+
+    An Anagram is a word or phrase formed by rearranging the letters of a differen
+    word or phrase, typically using all the original letters exactly once.
+
+    Input: strs = ["eat","tea","tan","ate","nat","bat"]
+    Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+    Input: strs = [""]
+    Output: [[""]]
+
+    Input: strs = ["a"]
+    Output: [["a"]]
+
+    Constraints:
+
+    1 <= strs.length <= 104
+    0 <= strs[i].length <= 100
+    strs[i] consists of lower-case English letters.
+
+    https://leetcode.com/problems/group-anagrams/
+    Date: 27 April 2021
+    San Diego, CA.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <limits.h>
+#include <string.h>  /* malloc */
+#include <stdbool.h>
+#include <math.h>
+#include <assert.h>
+#include <stdint.h> /* uint32_t */
+#include <unistd.h> /* sleep */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
