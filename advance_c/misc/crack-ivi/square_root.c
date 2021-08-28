62157aa3675655cca92dcd0b6f1138d2c7e5f8e0
Atul Raut <rauji.raut@gmail.com>
2021-08-28 10:42:38 -0700

    LeetCode: Add dynamic prog. questions.
    Add Maximum Profit in Job Scheduling using C.
    Add Longest Uncommon Subsequence II.
    Add Verify Preorder Serialization of a Binary Tree.
    
    Signed-off-by: Atul Raut <rauji.raut@gmail.com>

--- advance_c/misc/crack-ivi/square_root.c
+++ advance_c/misc/crack-ivi/square_root.c
@@ -1,6 +1,29 @@
+\***
+	Updated: 26 August 2021
+	Fair Oaks, CA, 95628
+*\
 #include<stdlib.h>
 #include<stdio.h>
 
+/* Simple one */
+int floorSqrt__(int x) {
+  // Base cases
+  if (x == 0 || x == 1)
+    return x;
+
+  // Staring from 1, try all
+  // numbers until i*i is
+  // greater than or equal to x.
+  int i = 1, result = 1;
+
+  while (result <= x) {
+    i++;
+    result = i * i;
+  }
+  return i - 1;
+}
+
+/* Optimized one : */
 // Returns floor of square root of x
 int floorSqrt(int x) {
   // Base cases
@@ -34,6 +57,6 @@ int main() {
 }
 
 /***
-=> ./a.out
- out = 6
+    => ./a.out
+    out = 6
 */
