1027e9e54058e1e628407859515cc93edcf59871
Atul Raut <rauji.raut@gmail.com>
2021-10-16 22:40:50 -0700

    LeetCode:tree: Add Same Tree
    Add Same Tree Using C
    
    Signed-off-by: Atul Raut <rauji.raut@gmail.com>

---
+++ tree_graph/leetcode/isSameTree.c
@@ -0,0 +1,85 @@
+/***
+    https://leetcode.com/problems/same-tree/
+    Same Tree
+
+    Given the roots of two binary trees p and q,
+    write a function to check if they are the
+    same or not.
+
+    Two binary trees are considered the same if
+    they are structurally identical, and the nodes
+    have the same value.
+
+    Input: p = [1,2,3], q = [1,2,3]
+    Output: true
+
+    Input: p = [1,2], q = [1,null,2]
+    Output: false
+
+    Input: p = [1,2,1], q = [1,1,2]
+    Output: false
+
+    Constraints:
+    The number of nodes in both trees is in the range [0, 100].
+    -104 <= Node.val <= 104
+
+    status: Pending
+    Date: Oct 17, 2021
+    Fair Oaks, CA.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <limits.h>
+#include <string.h>  /* malloc */
+#include <stdbool.h>
+#include <math.h>
+#include <assert.h>
+#include <stdint.h> /* uint32_t */
+#include <unistd.h> /* sleep */
+
+#define debug(str,args...) printf("[%s] L=%d :"str"\n", __func__, __LINE__, ##args)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q){
+  if (p == NULL && q == NULL) {
+    return true;
+  } else if (p == NULL || q == NULL) {
+    return false;
+  }
+
+  if (p->val == q->val) {
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+  } else {
+    return false;
+  }
+}
+
+int main (int argc, char **argv) {
+  int ret = 0;
+
+  debug("Output = %d", ret);
+  return 0;
+}
+
+
+/**
+   Complexity Analysis
+
+   Time complexity : \mathcal{O}(N)O(N), where N is a number of nodes in the tree, since one visits each node exactly once.
+
+   Space complexity : \mathcal{O}(\log(N))O(log(N)) in the best case of completely balanced tree and \mathcal{O}(N)O(N) in the worst case of completely unbalanced tree, to keep a recursion stack.
+**/
+
+/**
+
+ **/
