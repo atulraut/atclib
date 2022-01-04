6009e66908b409d3e44c5d504af776aa621ecdfa
Atul Raut <rauji.raut@gmail.com>
2022-01-03 20:34:58 -0800

    LeetCode: Find bitwise Complement for base 10
    Complement of Base 10 Integer
    
    Signed-off-by: Atul Raut <rauji.raut@gmail.com>

--- bitwise/leetcode/findComplement.c
+++ bitwise/leetcode/findComplement.c
@@ -50,10 +50,54 @@ int findComplement(int num) {
     return bitmask ^ num;
   }
 
+/**
+   https://leetcode.com/problems/complement-of-base-10-integer/
+   009. Complement of Base 10 Integer
+
+   The complement of an integer is the integer you get when
+   you flip all the 0's to 1's and all the 1's to 0's in its binary representation.
+
+   For example, The integer 5 is "101" in binary and its complement is "010" which is the integer 2.
+   Given an integer n, return its complement.
+
+   Input: n = 5
+   Output: 2
+   Explanation: 5 is "101" in binary, with complement "010" in binary, which is 2 in base-10.
+
+   Input: n = 7
+   Output: 0
+   Explanation: 7 is "111" in binary, with complement "000" in binary, which is 0 in base-10.
+
+   Input: n = 10
+   Output: 5
+   Explanation: 10 is "1010" in binary, with complement "0101" in binary, which is 5 in base-10.
+
+   Constraints:
+   0 <= n < 10^9
+   Date: Jan 3, 2022
+   Folsom, CA
+*/
+int bitwiseComplement(int N) {
+    if (N == 0) return 1;
+    int todo = N, bit = 1;
+    while (todo != 0) {
+      // flip current bit
+      N = N ^ bit;
+      // prepare for the next run
+      bit = bit << 1;
+      todo = todo >> 1;
+    }
+    return N;
+}
+
   int main () {
     int num = 5;
     //  printf ("\n -> %d \n", findComplement(num));
     printf ("\n Output -> %d \n", findComplement(num));
+
+    debug ("bitwiseComplement = %d", bitwiseComplement(5));
+
+    return 1;
   }
 
 /**
@@ -70,4 +114,8 @@ int findComplement(int num) {
    [findComplement] L=34 :result = 7 temp=0 final=2
 
    Output -> 2
+**/
+
+/**
+   [main] L=98 :bitwiseComplement = 2
 **/
