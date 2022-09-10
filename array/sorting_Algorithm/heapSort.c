e2c3c59cff9a3cd97c0868a22e99def460eab6e7
Atul Raut <rauji.raut@gmail.com>
2022-09-10 12:56:24 -0700

    stack_queue: Remove redudent file
    Signed-off-by: Atul Raut <rauji.raut@gmail.com>

--- array/sorting_Algorithm/heapSort.c
+++ array/sorting_Algorithm/heapSort.c
@@ -43,7 +43,19 @@
     __typeof__ (b) _b = (b);			\
     _a < _b ? _a : _b; })
 /*----------------------------------- Micro --------------------------------------*/
+void display_this(int* heap, int sz) {
+  debug ("Size of array = %d", sz);
+  for (int i=0; i<sz; ++i)
+    debug ("heap[%d] = %d", i, heap[i]);
+}
 
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+/* Max Heap */
 void heapify(int* heap, int curr, int size) {
   int largest = curr;
   int l = 2*curr+1;	//left child
@@ -54,33 +66,59 @@ void heapify(int* heap, int curr, int size) {
   if(r<size && heap[r] > heap[largest])
     largest = r;
 
-  if(largest!=curr) {
-    int temp      = heap[curr];
-    heap[curr]    = heap[largest];
-    heap[largest] = temp;
+  if(largest != curr) {
+    swap(&heap[curr], &heap[largest]);
 
     heapify(heap, largest, size);
   }
 }
 
+/**
+ * 0  1  2  3      4  5  6  7
+ * [3][6][5][0]    [8][2][1][9]
+ * |Internal Notes||  Leaves  |
+ *
+ * 		3
+ * 	 /	       \
+ * 	6		5
+ *   /      \        /       \
+ * 0 {A-}    8     2	      1
+ * |
+ * 9
+ * Start Heapify 0th Node {A-} first a & then Bottom Up
+ * Right to Left 0[idx-3] to 3[idx-0]
+ * */
 void heapSort(int* heap, int sz) {
-  // Build heap (rearrange array)
-  for (int i = sz / 2 - 1; i >= 0; i--)
-    heapify(heap, 0, i);
+  /**
+     Build heap (rearrange array, assume you got
+     input as array, ignore this par if its already heap)
+     Heapify the nodes near the leaves first
+     because their left & right subtree will
+     be following heap property & then hepify
+     parent internal notes (Bottom-Up)
+  */
+  /* Extram Map + Heapify Internal Nodes First */
+  for (int i = ((sz/2) - 1); i >= 0; i--) {
+    heapify(heap, i, sz);
+    display_this(heap, sz);
+  }
 
   for(int i=sz-1; i>0 ;--i) {
-    int temp = heap[0];	//Swap heap root with last element
-    heap[0] = heap[i];
-    heap[i] = temp;
+    //Swap heap root with last element
+    swap(&heap[0], &heap[i]);
 
     heapify(heap, 0, i);	//Heapify root with heapsize = i
   }
 }
 
+
 void test() {
   int ret = 0;
-  int heap[] = { 9, 6, 8, 2, 1, 4, 3};	//Max-Heap
-  int sz = 7;
+  int heap2[] = { 9, 6, 8, 2, 1, 4, 3};	//Max-Heap
+  //int heap[]  = { 5, 8, 3, 9, 4, 1, 7};
+  //  int heap[] = {54, 72, 45, 27, 36, 39, 99, 21, 63, 69};
+  int heap[] = {10, 9, 12, 6, 4, 7, 19, 13, 21, 39};
+  int sz = sizeof(heap)/sizeof(heap[0]);
   heapSort(heap, sz);
 
   debug("Heap in ASC is:\n");
@@ -110,4 +148,10 @@ int main (int argc, char **argv) {
    [test] L=76 :[5]--> [8]
 
    [test] L=76 :[6]--> [9]
+**/
+
+/**
+   Time Complexity = O(LogN)
+   SpaceComplexity = O(LogN)
+   Log N is Height of the tree
 **/
