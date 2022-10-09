0e77c9bfbb7e6a57a1016c45f5d630997836bffb
Atul Raut <rauji.raut@gmail.com>
2022-10-09 11:08:53 -0700

    Add whiteboard online web tool
    Signed-off-by: Atul Raut <rauji.raut@gmail.com>

--- tree_graph/crack-d-ivi/tree_arista.c
+++ tree_graph/crack-d-ivi/tree_arista.c
@@ -81,7 +81,6 @@ struct node* findPrev(struct node* n) {
 // 30, 25, 23, 21, 17, 12, 11, 9, 7, 6, 4, 2
 void printReverse(struct node* r) {
   // Your code here
-  printf ("\n");
   if (!r)
     return;
   // findMax => 30 ->print,
@@ -93,7 +92,6 @@ void printReverse(struct node* r) {
     printf ("->%d", curr->value);
     curr = findPrev(curr);
   }
-  printf ("\n");
 }
 
 void test() {
